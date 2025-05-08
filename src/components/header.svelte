ddd7a88a72c6efa33e29ad4e02c42d43cda6abb6
Abdoul Aziz BALDE <baldaazi99@gmail.com>
2025-05-08 18:03:15 +0200

    initial commit

---
+++ src/components/header.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import {locales, setLocale, getLocale} from "$lib/paraglide/runtime";
+    import {m} from "$lib/paraglide/messages";
+    let lang = getLocale();
+</script>
+
+<div class="flex justify-between">
+    <div>Hello</div>
+    <nav>
+        <ul class="flex gap-1.5 items-center">
+            <li><a href="#Home">{m["header.navigator.home"]()}</a></li>
+            <li><a href="#About">{m["header.navigator.about"]()}</a></li>
+            <li><a href="#Contact">{m["header.navigator.contact"]()}</a></li>
+        </ul>
+    </nav>
+    <select bind:value={lang} onchange={() => {setLocale(lang); console.log("eeeee");
+    }}>
+        {#each locales as local}
+            <option value={local}>{local.toUpperCase()}</option>
+        {/each}
+    </select>
+</div>
