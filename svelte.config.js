5c36c472323849cc755aa1657b32d546d2eabfba
Abdoul Aziz BALDE <baldaazi99@gmail.com>
2025-05-08 18:11:29 +0200

    define gh-pages bases

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,12 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { 
+		adapter: adapter(),
+		paths: {
+			base: prosses.env.ENV_MODE === "production"?"/sveltekit-github-pages":"",
+		}
+	 }
 };
 
 export default config;
